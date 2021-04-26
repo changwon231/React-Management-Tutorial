70495cb97bbac65584a0f2bbd0238aaa33bb4150
changwon231 <tlsckdnjs@naver.com>
2021-04-26 23:21:39 +0900

    Add Customer Delete Modeul

--- client/src/App.js
+++ client/src/App.js
@@ -113,6 +113,8 @@ class App extends Component {
                 <TableCell>생년월일</TableCell>
                 <TableCell>성별</TableCell>
                 <TableCell>직업</TableCell>
+                <TableCell>설정</TableCell>
+                
               </TableRow>
             </TableHead>
             <TableBody>
@@ -120,6 +122,7 @@ class App extends Component {
                 this.state.customers ? this.state.customers.map(c=> {
                   return (
                     <Customer
+                    stateRefresh={this.stateRefresh}
                     key={c.id}
                     id={c.id}
                     image={c.image}
